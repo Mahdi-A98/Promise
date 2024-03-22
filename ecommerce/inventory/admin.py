6504fb86561a89be8d6bdc8b3076d2edc216bdb6
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2024-03-22 23:18:54 +0330

    chore: registering models in admin panel

--- ecommerce/inventory/admin.py
+++ ecommerce/inventory/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import (
+    Product, ProductType, ProductInventory,
+    Category, Brand, Stock, Media,
+    ProductAttributeValues, ProductTypeAttribute
+    )
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Media)
+admin.site.register(Stock)
+admin.site.register(Product)
+admin.site.register(ProductInventory)
+admin.site.register(ProductType)
+admin.site.register(ProductAttributeValues)
+admin.site.register(ProductTypeAttribute)
