fe4527673e0b367e14b358a22b0d821adcd8a219
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2024-03-22 23:21:02 +0330

    fix: fix wrong inheritance

--- ecommerce/inventory/models.py
+++ ecommerce/inventory/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from mptt.models import TreeForeignKey
+from mptt.models import TreeForeignKey, MPTTModel
 
 # Create your models here.
 
 
-class Category(models.Model):
+class Category(MPTTModel):
     name = models.CharField(verbose_name=_("name"), max_length=100)
     slug = models.SlugField(verbose_name=_("slug"), max_length=150, unique=True)
     is_active = models.BooleanField(verbose_name=_("is active"), default=False)
@@ -19,6 +19,10 @@ class Category(models.Model):
         db_table = "category"
         verbose_name = _("category")
         verbose_name_plural = _("categories")
+
+    
+    def __str__(self) -> str:
+        return self.name
 
 
 class Product(models.Model):
