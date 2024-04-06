712a1ffba93686be2eff7019721e2b97e9635e07
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2024-04-06 18:37:31 +0330

    chore: adding main vue component including navbar and footer

---
+++ front_end/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>Promise shop</strong></router-link>
+        
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @mouseleave="changeBurger" @click="changeBurger">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
+        <div class="navbar-end">
+          <router-link to="/summer" class="navbar-item">Summer</router-link>
+          <router-link to="/winter" class="navbar-item">Winter</router-link>
+        </div>
+        <div class="navbar-item">
+          <div class="buttons">
+            <router-link to="/login" class="button is-light">Log in</router-link>
+            <router-link to="/login" class="button is-success">
+              <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+              <span>Cart</span>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <section class="section">
+      <router-view/>
+    </section>
+
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2021</p>
+    </footer>
+  </div>
+</template>
