918e0a356dda46ea7bfdf53a99e7b609672a0722
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2024-05-23 12:57:42 +0330

    chore: as we moved selective attribute to a n explicite component file we need to define props to make update seection or other functionalities available to woek with parent component or app

--- front_end/src/components/selectiveAttribute.vue
+++ front_end/src/components/selectiveAttribute.vue
@@ -1,6 +1,7 @@
 # In the name of GOD
+<!-- # <div class="box m-2 column is-5 is-10-tablet bg-purple-100 focus:ring-offset-4 focus:ring-4 hover:bg-purple-200 focus:ring-purple-600" :class="{'selectedd': false}" :isSelected="true"> -->
 <template>
-    <div class="box m-2 column is-5 is-10-tablet bg-purple-100 focus:ring-offset-4 focus:ring-4 hover:bg-purple-200 focus:ring-purple-600" :class="{'selectedd': false}" :isSelected="true">
+    <div :class="{'is-3 is-tablet-5': manySiblings, 'is-5 is-10-tablet': !manySiblings}" class="box my-2 ml-1 column bg-purple-100 focus:ring-offset-4 focus:ring-4 hover:bg-purple-200 focus:ring-purple-600" :isSelected="true">
             <h3>{{ attr?.name }}</h3>
             <div v-for="value in attr.attributes" class="m-2 is-flex is-flex-directions-column is-justify-content-space-around">
                 <div class="box column is-10 is-justify-content-space-around is-flex mt-2 bg-rose-100"  @click="updateSelection(attr?.name, value.attribute_value)" :class="{'border-dashed ring-purple-900 ring-2 ring-offset-2':value?.isSelected}">
@@ -24,5 +25,5 @@
 
 <script setup>
 import { onMounted, getCurrentInstance } from 'vue';
-defineProps(['attr', 'updateSelection'])
+defineProps(['attr', 'updateSelection', 'manySiblings'])
 </script>
