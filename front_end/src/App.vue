63c0ab3033d3fa78a487f41b3db8508f02d613e6
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2024-05-23 12:45:22 +0330

    chore: implementing loading bar to display when fetching data is slow

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -4,19 +4,28 @@
     <Header/>
     
     <section class="section">
+      <div class="is-loading-bar has-text-centered" v-bind:class="{'is-loading': store.state.isLoading}">
+        <div class="lds-dual-ring"></div>
+      </div>
       <router-view/>
     </section>
 
-    <footer class="footer">
-      <p class="has-text-centered">Copyright (c) 2021</p>
-    </footer>
-
+    
   </div>
+  <footer class="footer">
+    <p class="has-text-centered">Copyright (c) 2021</p>
+  </footer>
 </template>
 
 <script setup>
-  import {ref} from 'vue';
+  import {ref, onBeforeMount } from 'vue';
   import Header from "@/components/Header.vue";
+  import store from './store';
+
+  onBeforeMount(() => {
+    console.log("onBeforMount")
+    store.commit('initializeStore');
+  })
 </script>
 
 
