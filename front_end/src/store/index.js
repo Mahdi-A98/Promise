20b636a5f7e55ccade77236017ec987d9d8cadf5
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2024-05-23 13:18:42 +0330

    chore: add to cart function and initializing cart function at vue store

--- front_end/src/store/index.js
+++ front_end/src/store/index.js
@@ -12,6 +12,28 @@ export default createStore({
   getters: {
   },
   mutations: {
+    initializeStore(state) {
+      if (localStorage.getItem('cart')) {
+        state.cart = JSON.parse(localStorage.getItem('cart'));
+      }
+      else {
+        localStorage.setItem('cart', JSON.stringify(state.cart));
+      }
+    },
+    addToCart(state, item) {
+      const exists = state.cart.items.filter(i => i.product.sku === item.product.sku);
+      if (exists.length) {
+        console.log(JSON.stringify(exists[0]))
+        exists[0].quantity = parseInt(exists[0].quantity) + parseInt(item.quantity);
+      }
+      else {
+        state.cart.items.push(item);
+      }
+      localStorage.setItem('cart', JSON.stringify(state.cart));
+    },
+    setIsLoading(state, status) {
+      state.isLoading = status;
+    }
   },
   actions: {
   },
